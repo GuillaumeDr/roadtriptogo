e575d16b5cfbaed756f534437cb5d37aa3c0d15c
Simon Chretien <simon.chretien62@gmail.com>
2022-06-03 12:05:37 +0200

    Add steps

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -8,15 +8,23 @@ export default class extends Controller {
   }
 
   connect() {
+    this.steps = ``
+    this.markersValue.forEach((marker) => {
+      const step = [marker.lng, marker.lat]
+      this.steps += `${step};`
+    })
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+    this.#addRoute();
   }
+
+
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
@@ -30,74 +38,58 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
-}
-
-function updateRoute() {
-  // Set the profile
-  const profile = 'driving';
-  // Get the coordinates that were drawn on the map
-  const data = draw.getAll();
-  const lastFeature = data.features.length - 1;
-  const coords = data.features[lastFeature].geometry.coordinates;
-  // Format the coordinates
-  const newCoords = coords.join(';');
-  // Set the radius for each coordinate pair to 25 meters
-  const radius = coords.map(() => 25);
-  getMatch(newCoords, radius, profile);
-}
 
 
-// Draw the Map Matching route as a new layer on the map
-function addRoute(coords) {
-  // If a route is already loaded, remove it
-  if (map.getSource('route')) {
-    map.removeLayer('route');
-    map.removeSource('route');
-  } else {
-    // Add a new layer to the map
-    map.addLayer({
-      id: 'route',
-      type: 'line',
-      source: {
-        type: 'geojson',
-        data: {
+  #drawRoute(coord){
+    const geojson = {
           type: 'Feature',
           properties: {},
-          geometry: coords
+          geometry: {
+            type: 'LineString',
+            coordinates: coord
+          }
+        };
+        if (this.map.getSource('route')) {
+          this.map.getSource('route').setData(geojson);
         }
-      },
-      layout: {
-        'line-join': 'round',
-        'line-cap': 'round'
-      },
-      paint: {
-        'line-color': '#03AA46',
-        'line-width': 8,
-        'line-opacity': 0.8
-      }
-    });
-  }
-}
+        else {
+          this.map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: {
+              type: 'geojson',
+              data: geojson
+            },
+            layout: {
+              'line-join': 'round',
+              'line-cap': 'round'
+            },
+            paint: {
+              'line-color': 'red',
+              'line-width': 5,
+              'line-opacity': 1
+            }
+          });
+        }
 
-// Make a Map Matching request
-async function getMatch(coordinates, radius, profile) {
-  // Separate the radiuses with semicolons
-  const radiuses = radius.join(';');
-  // Create the query
-  const query = await fetch(
-    `https://api.mapbox.com/matching/v5/mapbox/${profile}/${coordinates}?geometries=geojson&radiuses=${radiuses}&steps=true&access_token=${mapboxgl.accessToken}`,
-    { method: 'GET' }
-  );
-  const response = await query.json();
-  // Handle errors
-  if (response.code !== 'Ok') {
-    alert(
-      `${response.code} - ${response.message}.\n\nFor more information: https://docs.mapbox.com/api/navigation/map-matching/#map-matching-api-errors`
-    );
-    return;
   }
-  // Get the coordinates from the response
-  const coords = response.matchings[0].geometry;
-  // Draw the route on the map
-  addRoute(coords);
+
+  #addRoute() {
+    if (this.steps.length > 21) {
+      console.log("hello")
+      // async function getRoute() {
+        fetch(
+          `https://api.mapbox.com/directions/v5/mapbox/driving/${this.steps.substring(0, this.steps.length - 1)}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`)
+          .then(response => response.json())
+          .then(data =>  {
+            const coord = data.routes[0].geometry.coordinates;
+            console.log(coord);
+            this.#drawRoute(coord);
+          });
+
+        //
+
+    }
+
+  }
 }
