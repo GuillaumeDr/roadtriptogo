da604acc3c0056ba8b348e3e53a218b6d17133d4
guillaume.drohe@gmail.com <guillaume.drohe@gmail.com>
2022-06-07 09:45:44 +0200

    fix picture colabo

--- app/javascript/controllers/add_collab_controller.js
+++ app/javascript/controllers/add_collab_controller.js
@@ -14,12 +14,11 @@ export default class extends Controller {
     if (target.dataset.selected === "false"){
       target.dataset.selected = "true"
       this.inputTarget.value += target.dataset.userid +","
-      console.log(target.dataset.userkey)
 
       if (target.dataset.userkey == null) {
         this.collaboratorsTarget.insertAdjacentHTML("beforeend",`<img class="avatar avatar-collab" id=${idcollab} data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" src="https://images.unsplash.com/photo-1608889335941-32ac5f2041b9?ixlib=rb-1.2.1&amp;raw_url=tr[…]MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=580">`)
       } else {
-        var htmlUrl = `http://res.cloudinary.com/dji262zcw/image/upload/v1/development/${target.dataset.userkey}`
+        var htmlUrl = target.src
         this.collaboratorsTarget.insertAdjacentHTML("beforeend",`<img class="avatar avatar-collab" id=${idcollab} width="45" src=${htmlUrl}>`)
       }
     }else{
