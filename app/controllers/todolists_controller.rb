ec7d78206551be0dd65ecff50f40de052e00743e
Laura Petit <lau.petit0@gmail.com>
2022-06-07 09:23:10 +0200

    ntg

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,10 +1,7 @@
 class TodolistsController < ApplicationController
-
-
   def show
     @project = Project.find(params[:project_id])
     @tasks = @project.todolist.tasks
     @todolist = Todolist.find(params[:id])
   end
-
 end
